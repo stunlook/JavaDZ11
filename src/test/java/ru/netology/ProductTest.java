3b44a151f5590f2e258b969bcc346a52ce956a54
Stepan Smirnov <stunlooksteff@gmail.com>
2022-12-18 15:45:38 +0300

    New Test

--- src/test/java/ru/netology/ProductTest.java
+++ src/test/java/ru/netology/ProductTest.java
@@ -10,50 +10,50 @@ public class ProductTest {
     public void testAddAllBooks() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1,"HarryPotter",100,"Joanne Rowling");
-        Book book2 = new Book(2,"HarryPotter2",200,"Joanne Rowling");
-        Book book3 = new Book(3,"The Lord of the Rings1",300,"John Ronald Reuel Tolkien");
-        Book book4 = new Book(4,"The Lord of the Rings2",150,"John Ronald Reuel Tolkien");
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+        Book book2 = new Book(2, "HarryPotter2", 200, "Joanne Rowling");
+        Book book3 = new Book(3, "The Lord of the Rings1", 300, "John Ronald Reuel Tolkien");
+        Book book4 = new Book(4, "The Lord of the Rings2", 150, "John Ronald Reuel Tolkien");
 
         manager.add(book1);
         manager.add(book2);
         manager.add(book3);
         manager.add(book4);
 
-        Product[] expected = {book1,book2,book3,book4};
+        Product[] expected = {book1, book2, book3, book4};
         Product[] actual = repo.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testAddOneBookOneSmartphone() {
+    public void testAddOneBookAddOneSmartphone() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1,"HarryPotter",100,"Joanne Rowling");
-        Book book2 = new Book(2,"HarryPotter2",200,"Joanne Rowling");
-        Book book3 = new Book(3,"The Lord of the Rings1",300,"John Ronald Reuel Tolkien");
-        Book book4 = new Book(4,"The Lord of the Rings2",150,"John Ronald Reuel Tolkien");
-        Smartphone phone1 = new Smartphone(5,"iphone 14","Apple",1000);
-        Smartphone phone2 = new Smartphone(6,"Galaxy s20 FE","Samsung",1500);
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+        Book book2 = new Book(2, "HarryPotter2", 200, "Joanne Rowling");
+        Book book3 = new Book(3, "The Lord of the Rings1", 300, "John Ronald Reuel Tolkien");
+        Book book4 = new Book(4, "The Lord of the Rings2", 150, "John Ronald Reuel Tolkien");
+        Smartphone phone1 = new Smartphone(5, "iphone 14", "Apple", 1000);
+        Smartphone phone2 = new Smartphone(6, "Galaxy s20 FE", "Samsung", 1500);
 
         manager.add(book1);
         manager.add(phone2);
 
-        Product[] expected = {book1,phone2};
+        Product[] expected = {book1, phone2};
         Product[] actual = repo.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testSearch() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1,"HarryPotter",100,"Joanne Rowling");
-        Book book2 = new Book(2,"HarryPotter2",200,"Joanne Rowling");
-        Book book3 = new Book(3,"The Lord of the Rings1",300,"John Ronald Reuel Tolkien");
-        Book book4 = new Book(4,"The Lord of the Rings2",150,"John Ronald Reuel Tolkien");
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+        Book book2 = new Book(2, "HarryPotter2", 200, "Joanne Rowling");
+        Book book3 = new Book(3, "The Lord of the Rings1", 300, "John Ronald Reuel Tolkien");
+        Book book4 = new Book(4, "The Lord of the Rings2", 150, "John Ronald Reuel Tolkien");
 
 
         manager.add(book1);
@@ -61,22 +61,22 @@ public class ProductTest {
         manager.add(book3);
         manager.add(book4);
 
-        Product[] expected = {book1,book2};
+        Product[] expected = {book1, book2};
         Product[] actual = manager.searchBy("HarryPotter");
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testRemoveBookAndSmartphoneRepo() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1,"HarryPotter",100,"Joanne Rowling");
-        Book book2 = new Book(2,"HarryPotter2",200,"Joanne Rowling");
-        Book book3 = new Book(3,"The Lord of the Rings1",300,"John Ronald Reuel Tolkien");
-        Book book4 = new Book(4,"The Lord of the Rings2",150,"John Ronald Reuel Tolkien");
-        Smartphone phone1 = new Smartphone(5,"iphone 14","Apple",1000);
-        Smartphone phone2 = new Smartphone(6,"Galaxy s20 FE","Samsung",1500);
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+        Book book2 = new Book(2, "HarryPotter2", 200, "Joanne Rowling");
+        Book book3 = new Book(3, "The Lord of the Rings1", 300, "John Ronald Reuel Tolkien");
+        Book book4 = new Book(4, "The Lord of the Rings2", 150, "John Ronald Reuel Tolkien");
+        Smartphone phone1 = new Smartphone(5, "iphone 14", "Apple", 1000);
+        Smartphone phone2 = new Smartphone(6, "Galaxy s20 FE", "Samsung", 1500);
 
 
         manager.add(book1);
@@ -91,22 +91,22 @@ public class ProductTest {
         repo.removeById(5);
 
 
-        Product[] expected = {book3,book4,phone2};
+        Product[] expected = {book3, book4, phone2};
         Product[] actual = repo.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void testRemoveBookAndSmartphoneManager() {
         ProductRepository repo = new ProductRepository();
         ProductManager manager = new ProductManager(repo);
-        Book book1 = new Book(1,"HarryPotter",100,"Joanne Rowling");
-        Book book2 = new Book(2,"HarryPotter2",200,"Joanne Rowling");
-        Book book3 = new Book(3,"The Lord of the Rings1",300,"John Ronald Reuel Tolkien");
-        Book book4 = new Book(4,"The Lord of the Rings2",150,"John Ronald Reuel Tolkien");
-        Smartphone phone1 = new Smartphone(5,"iphone 14","Apple",1000);
-        Smartphone phone2 = new Smartphone(6,"Galaxy s20 FE","Samsung",1500);
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+        Book book2 = new Book(2, "HarryPotter2", 200, "Joanne Rowling");
+        Book book3 = new Book(3, "The Lord of the Rings1", 300, "John Ronald Reuel Tolkien");
+        Book book4 = new Book(4, "The Lord of the Rings2", 150, "John Ronald Reuel Tolkien");
+        Smartphone phone1 = new Smartphone(5, "iphone 14", "Apple", 1000);
+        Smartphone phone2 = new Smartphone(6, "Galaxy s20 FE", "Samsung", 1500);
 
 
         manager.add(book1);
@@ -121,10 +121,35 @@ public class ProductTest {
         manager.removeById(5);
 
 
-        Product[] expected = {book2,book4,phone2};
+        Product[] expected = {book2, book4, phone2};
         Product[] actual = repo.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void TestOneProduct() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "HarryPotter", 100, "Joanne Rowling");
+
+        manager.add(book1);
+
+        Product[] expected = {book1};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testNullProduct() {
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+
+        Product[] expected = {};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
